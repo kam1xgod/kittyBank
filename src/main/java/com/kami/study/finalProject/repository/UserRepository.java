9bb59c46fcc3a4daceba248ed101da945532f2fd
kami <kamixtrash@gmail.com>
2022-04-20 15:30:04 +0400

    add finByPhoneNumber method in UserRepository.

--- src/main/java/com/kami/study/finalProject/repository/UserRepository.java
+++ src/main/java/com/kami/study/finalProject/repository/UserRepository.java
@@ -4,6 +4,9 @@ import com.kami.study.finalProject.model.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByPhoneNumber(String phoneNumber);
 }
