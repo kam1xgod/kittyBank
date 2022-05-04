613b096dd7674ea454a7547941a69684662208ae
kami <kamixtrash@gmail.com>
2022-05-04 13:55:28 +0400

    get rid of packages in controller package.

--- src/main/java/com/kami/study/finalProject/controller/account/AccountController.java
+++
@@ -1,9 +0,0 @@
-package com.kami.study.finalProject.controller.account;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/api/v1/account/")
-public class AccountController {
-}
--- src/main/java/com/kami/study/finalProject/controller/transfer/TransferController.java
+++
@@ -1,9 +0,0 @@
-package com.kami.study.finalProject.controller.transfer;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/api/v1/transfer")
-public class TransferController {
-}
