8e5af7ed77a2a0b08a90cec045fdc90faf974b3c
kami <kamixtrash@gmail.com>
2022-07-14 17:21:20 +0400

    Fix service method for requesting new credit account.
    It was kinda wrong, because every time DB was trying to add new
    request, when there're already was one for particular email.
    After all, now it doing nothing. Just returns request already in DB.

--- src/main/java/com/kami/study/finalProject/service/persistence/impl/CreditAccountRequestServiceImpl.java
+++ src/main/java/com/kami/study/finalProject/service/persistence/impl/CreditAccountRequestServiceImpl.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -35,8 +36,12 @@ public class CreditAccountRequestServiceImpl implements CreditAccountRequestServ
 
     @Override
     public CreditAccountRequest create(CreditAccountRequest request) {
-        requestRepository.save(request);
-        return request;
+        CreditAccountRequest existedRequest = requestRepository.findByUser_Mail(request.getUser().getMail()); 
+        if (Objects.isNull(existedRequest)) {
+          requestRepository.save(request);
+          return request;
+        }
+        return existedRequest;
     }
 
     @Override
